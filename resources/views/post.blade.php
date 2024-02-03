33e3aaeaf7a05458b8c2d9fc6e0f9de3caf00ebd
I Made Satria Bimantara <satriabimantara.md@gmail.com>
2024-02-04 00:12:14 +0800

    Initial commit for Laravel8.5.0 coding tutorial

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('container')
+<article class="mb-5">
+    <h2>
+        Title
+    </h2>
+    <h5>Autor</h5>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quo quis assumenda, rerum totam nemo cumque, a ut accusantium vitae perspiciatis nisi at, temporibus dolor saepe inventore! Illum in cumque, dolore corporis aliquam enim laudantium commodi molestiae veniam ipsum necessitatibus consequuntur dicta quia similique. Corporis, nulla alias? Accusantium ad deserunt obcaecati, provident labore eos aspernatur hic ea ipsa atque eius fugit, repellendus rerum? Veritatis, impedit debitis eligendi iste laboriosam quod corporis, hic eos, eum repudiandae nam. Iste amet enim fuga nisi odio dolore tempora quaerat unde fugit. Tempore libero, eius similique harum soluta quia voluptate modi iste reprehenderit corrupti sunt necessitatibus ipsa cumque neque non ipsum maiores at molestiae dignissimos ab! Incidunt commodi labore iure mollitia magni illo in voluptatum alias ipsa, corrupti rem ullam, hic eos laboriosam? Optio itaque autem reiciendis recusandae dignissimos, nobis asperiores, voluptatem perspiciatis sapiente quos eius qui quidem odit rem. Autem iste voluptates pariatur vitae cupiditate eos vero sed omnis porro similique laudantium reiciendis aspernatur, dolorum sint quam tempora mollitia! Recusandae aut est nemo eos facere exercitationem! Ullam sit ut velit. Incidunt nihil nostrum minus, similique recusandae est earum cumque tempora officia sit optio ab pariatur reprehenderit, officiis voluptas. Fugiat qui iure sapiente ad amet.
+    </p>
+</article>
+<a href="/blog" class="btn btn-primary btn-sm">Back to Blog</a>
+@endsection
